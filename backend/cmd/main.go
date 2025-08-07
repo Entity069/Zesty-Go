bdcf99347c2e048347e960d108b20755b460eec1
Entity069 <entity069@protonmail.com>
2025-08-08 00:51:47 +0530

    chore: add main.go

---
+++ backend/cmd/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/joho/godotenv"
+
+	"github.com/Entity069/Zesty-Go/pkg/api"
+	"github.com/Entity069/Zesty-Go/pkg/config"
+	"github.com/Entity069/Zesty-Go/pkg/models"
+)
+
+func main() {
+	_ = godotenv.Load()
+
+	models.InitDB()
+	defer func() {
+		if err := models.CloseDB(); err != nil {
+			log.Printf("fuck: error closing DB: %v", err)
+		}
+	}()
+
+	router := api.NewRouter()
+	addr := config.SiteName()
+
+	srv := &http.Server{
+		Addr:              addr,
+		Handler:           router,
+		ReadHeaderTimeout: 10 * time.Second,
+	}
+
+	go func() {
+		log.Printf("app listening on %s", addr)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("fuck: server error: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	log.Println("shutting down...")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Printf("fuck: graceful shutdown failed: %v", err)
+	}
+	log.Println("server exited")
+}
